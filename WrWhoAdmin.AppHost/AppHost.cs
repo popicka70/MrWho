16309dcb298d668f8cb65e514cbc26f5960c2a3e
popicka70 <74075683+popicka70@users.noreply.github.com>
2025-08-03 18:42:12 +0200

    Remove unused API

--- WrWhoAdmin.AppHost/AppHost.cs
+++ WrWhoAdmin.AppHost/AppHost.cs
@@ -5,17 +5,14 @@ var sqlServer = builder.AddSqlServer("sqlserver")
     .WithDataVolume()
     .AddDatabase("mrwhodb");
 
-var apiService = builder.AddProject<Projects.MrWhoAdmin_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
-
-builder.AddProject<Projects.MrWhoAdmin_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
-builder.AddProject<Projects.MrWho>("mrwho")
+var mrWho = builder.AddProject<Projects.MrWho>("mrwho")
     .WithReference(sqlServer)
     .WaitFor(sqlServer);
+
+var adminWeb = builder.AddProject<Projects.MrWhoAdmin_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(mrWho)
+    .WaitFor(mrWho);
 
 builder.Build().Run();
