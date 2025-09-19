0594e67ed3d3e873baca255721822994353de8b0
popicka70 <74075683+popicka70@users.noreply.github.com>
2025-09-19 17:08:46 +0200

    Add missing migration

---
+++ MrWho/Migrations/20250919150748_KeepItUpToDate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MrWho.Migrations
+{
+    /// <inheritdoc />
+    public partial class KeepItUpToDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PushedAuthorizationRequests_ClientId_ParametersHash",
+                table: "PushedAuthorizationRequests",
+                columns: new[] { "ClientId", "ParametersHash" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PushedAuthorizationRequests_ClientId_ParametersHash",
+                table: "PushedAuthorizationRequests");
+        }
+    }
+}
